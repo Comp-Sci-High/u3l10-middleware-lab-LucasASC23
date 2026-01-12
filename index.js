14e7cf4f968c4828a98f528b3ec382911723a95c
LucasASC23 <139932248+LucasASC23@users.noreply.github.com>
2026-01-12 03:43:01 +0000

    Finshed lab

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // INSTRUCTIONS: Write the code that does the following:
 // -0 Set up the server correctly
+const express=require("express")
+const app=express()
 // -1 console logs the method and URL for every request
 // -2 sends back "Welcome to the Animal Shelter Network" as an h1 when the client goes to /
 // -3 sends back the cat object when the client goes to /api/cat
@@ -22,3 +24,35 @@ const animalShelterData = {
         { name: "Cozy Paws Sanctuary", location: "456 Oak Avenue, Townsburg" }
     ]
 };
+
+app.use((req,res,next)=>{
+    console.log(req.method+" "+req.url)
+    next()
+})
+
+app.get("/",(req,res)=>{
+    res.status(200).json("<h1>Welcome to the Animal Shelter Network</h1>")
+})
+app.get("/api/cat",(req,res)=>{
+    res.status(200).json(animalShelterData.cats)
+})
+
+app.get("/api/shelters",(req,res)=>{
+    res.status(200).json(animalShelterData.shelters)
+})
+
+app.get("/info",(req,res)=>{
+    res.status(200).send("Go to /api/cat to see cats for adoption and /api/shelters to see shelters")
+})
+
+app.get("/api/first-cat", (req,res)=>{
+    const firstCatName=animalShelterData.cats[0].name
+    res.status(200).send("A cat you can adopt is "+firstCatName)
+})
+
+app.use((req,res,next)=>{
+    res.status(404).send("404 Page Not Found")
+})
+app.listen(3000,()=>{
+    console.log("server running")
+})
